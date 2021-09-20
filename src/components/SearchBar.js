171e26c0f7de96a0adaed150ff2ff9c6ddf6f57f
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2021-09-20 16:53:56 -0300

    3 requisito

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -8,6 +8,7 @@ class SearchBar extends React.Component {
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+
       </div>
     );
   }
