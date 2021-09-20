874d727d603cc6a1cbac92b8803e4d6c8ef2bdf4
Bel-Albuquerque <mariaisabelalbuquerque@gmail.com>
2021-09-20 18:03:01 -0300

    correcao lint

--- src/components/FilterCategories.js
+++ src/components/FilterCategories.js
@@ -1,21 +1,24 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 
 class FillterCatergories extends React.Component {
   render() {
     const { products } = this.props;
-    return( 
-        <ul>
-            {products.map(({ id, name }) => (
-                <li data-testid="category" key={id}>{name}</li>
+    return (
+      <ul>
+        {products.map(({ id, name }) => (
+          <li data-testid="category" key={ id }>{name}</li>
 
-            )
-            ) }
-        </ul>
-    ) 
+        )) }
+      </ul>
+    );
   }
-
-  
 }
+
+FillterCatergories.propTypes = {
+  products: PropTypes.arrayOf(
+    PropTypes.object,
+  ).isRequired,
+};
 
 export default FillterCatergories;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,40 +2,41 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import SearchBar from '../components/SearchBar';
 import FillterCatergories from '../components/FilterCategories';
-import {getCategories} from '../services/api';
+import { getCategories } from '../services/api';
 
 class Home extends React.Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
       categories: [],
       requestCategoriesApi: true,
-    }
+    };
   }
-  
+
   componentDidMount() {
     this.funcGetCategories();
   }
 
   funcGetCategories = async () => {
-    const {requestCategoriesApi} = this.state
+    const { requestCategoriesApi } = this.state;
     if (requestCategoriesApi) {
-      const categories = await getCategories ()    
+      const categories = await getCategories();
       this.setState({
-      categories, 
-      requestCategoriesApi: false,
-      })
+        categories,
+        requestCategoriesApi: false,
+      });
     }
   }
+
   render() {
-    const {categories, requestCategoriesApi} = this.state; 
+    const { categories, requestCategoriesApi } = this.state;
     return (
       <div>
         <SearchBar />
         <Link data-testid="shopping-cart-button" to="/shoppingcart">
           <img src="../../shopping-cart-1985.png" alt="carrinho" />
         </Link>
-        {!requestCategoriesApi && <FillterCatergories products={categories}/>}
+        {!requestCategoriesApi && <FillterCatergories products={ categories } />}
       </div>
     );
   }
