946a63065c9127f5a4eff904bba44a9074887466
Jean Marcel <jeanvanzella91@gmail.com>
2021-09-20 15:05:39 -0300

    req 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
 export async function getCategories() {
-  // Implemente aqui
+  const fecthCategories = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const fecthCategoriesJson = await fecthCategories.json();
+  return fecthCategoriesJson;
 }
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  const FetchProducts = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query} `);
+  const FetchProductsJson = await FetchProducts.json();
+  return FetchProductsJson;
 }
